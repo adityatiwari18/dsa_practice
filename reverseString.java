9e13f9c25f6fa78bb8fa2453f72a7ff844cc518e
Aditya Tiwari <tiwari.adityacool@gmail.com>
2021-01-03 19:04:01 +0530

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseWord(str));
+        }
+    }
+}
+class Reverse
+{
+    public static String reverseWord(String str)
+    {
+        String s="";
+        for(int i=0; i<str.length();i++){
+            s+=str.charAt(str.length()-1-i);
+        }
+        return s;
+    }
+}
