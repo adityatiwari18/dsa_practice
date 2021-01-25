903f46ed8733ba1d16129102ed8a223ec7208b79
adityatiwari18 <tiwari.adityacool@gmail.com>
2021-01-25 22:58:55 +0530

    Program code to find Longest common subseq

---
+++ String/LongestSubseq.java
@@ -0,0 +1,40 @@
+import java.io.*;
+class LongestSubseq
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String str = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.LongestRepeatingSubsequence(str);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int LongestRepeatingSubsequence(String str)
+    {
+        String res="";
+        for(int i=0;i<str.length();i++){
+            String temp="";
+            for(int j=i+1;j<str.length();j++){
+                if(str.charAt(i)==str.charAt(j)){
+                    temp = temp + str.charAt(j);
+                }
+            }
+            if(temp.length()>res.length())
+                res=temp;
+        }
+        
+        return res.length();
+    }
+}
