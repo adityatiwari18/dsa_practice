2de7540ce9e2c815605e43bef1de40373d12a9f4
Aditya Tiwari <tiwari.adityacool@gmail.com>
2021-02-15 15:41:55 +0530

    Program to reverse doubly Linked List

---
+++ LinkedList/ReverseDLL.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+class ReverseDLL
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            String str[] = read.readLine().trim().split(" ");
+            Node head = null, tail = null;
+            for(int i = 0; i < n; i++)
+            {
+                int data = Integer.parseInt(str[i]);
+                if (head == null) 
+                {
+                    head = new Node(data);
+                    tail = head;
+                }
+                else
+                {
+                    tail.next = new Node(data);
+                    tail.next.prev=tail;
+                    tail = tail.next;
+                }
+            }
+            head=reverseDLL(head);
+            if(verify(head))
+    	    displayList(head);
+    	    else
+    	    System.out.print("Your pointers are not correctly connected");
+            System.out.println();
+                
+        }
+    }
+   
+public static void displayList(Node head)
+{
+    while(head.next != null)
+    {
+        System.out.print(head.data+" ");
+        head = head.next;
+    }
+    System.out.print(head.data);
+   
+}
+
+public static Node reverseDLL(Node  head)
+{
+    Node cur=head;
+    Node pre = null;
+    while(cur!=null){
+        Node nex = cur.next;
+        cur.next = pre;
+        cur.prev=nex;
+        pre = cur;
+        cur = nex;
+    }
+    return pre;
+}
+
+public static boolean verify(Node head)
+{
+    int fl=0;
+    int bl=0;
+    
+    Node temp=head;
+    
+    while(temp.next!=null)
+    {
+        temp=temp.next;
+        fl++;
+    }
+    
+    while(temp.prev!=null)
+    {
+        temp=temp.prev;
+        bl++;
+    }
+    
+    return fl==bl;
+    }
+}
