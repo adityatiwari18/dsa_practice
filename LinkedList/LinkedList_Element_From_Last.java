d6be7620e7dce50533c82ea1850ccf9afde44f24
Aditya Tiwari <tiwari.adityacool@gmail.com>
2021-02-23 15:10:45 +0530

    Nth node from end of linked list

---
+++ LinkedList/LinkedList_Element_From_Last.java
@@ -0,0 +1,91 @@
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) 
+    {
+        data = d; 
+        next = null;
+    }
+}
+	
+
+	
+public class LinkedList_Element_From_Last
+{
+    Node head;  
+	  Node tail;
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+    public void addToTheLast(Node node) 
+	  {
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+    public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			LinkedList_Element_From_Last llist = new LinkedList_Element_From_Last();
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+          
+		//System.out.println(llist.head.data);
+        GfG g = new GfG(); 
+		//System.out.println(k);
+		System.out.println(g.getNthFromLast(llist.head,k));
+		
+			t--;
+		}
+	}
+}
+
+class GfG
+{
+    int getNthFromLast(Node head, int n)
+    {
+        if(head==null)
+            return -1;
+    	Node cur=head;
+    	int c=0;
+    	while(cur!=null){
+    	    ++c;
+    	    cur=cur.next;
+    	}
+    	if(n>c)
+    	    return -1;
+    	c=c-n;
+    	while(c-->0){
+    	    head=head.next;
+    	}
+    	return head.data;
+    }
+}
